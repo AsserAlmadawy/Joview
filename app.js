3ed5d21ee50ccd5561807c438712bde7544f6182
AsserAlmadawy <asser.almadawy@hotmail.com>
2025-08-30 13:09:49 +0300

    fixed vite problem with firebase

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import { jobs } from "./jobs.js";
+import { send } from "@emailjs/browser";
 
 const jobsBody = document.getElementById("jobs-body");
 const arrow = document.querySelector(".arrow");
@@ -603,3 +604,79 @@ window.addEventListener("scroll", () => {
 document.addEventListener("DOMContentLoaded", () => scrollTo({ top: 0, behavior: "smooth" }));
 
 jobsBody.style.height = "unset";
+
+// Contact Us
+
+const Name = document.getElementById("name");
+const email = document.getElementById("email");
+const nameHolder = document.getElementById("name-holder");
+const emailHolder = document.getElementById("email-holder");
+const message = document.querySelector("textarea");
+const messageHolder = document.getElementById("message-holder");
+const submit = document.getElementById("submit-btn");
+
+const sendEmail = e => {
+  e.preventDefault();
+  
+  if (Name.value === "" || email.value === "" || message.value === "") {
+    alert("Please enter your name, email, and message.");
+  } else {
+    if (!email.value.includes("@")) {
+      alert("Email must contain @ symbol.");
+    } else {
+     submit.textContent = "SENDING...";
+
+    send("joview-mail", "template_scnkg34", {
+    name: Name.value.trim(),
+    email: email.value.trim(),
+    message: message.value.trim()
+}, "XymwmkfziJI-JtCUJ")
+
+    .then(() => {
+      Name.value = "";
+      email.value = "";
+      message.value = "";
+
+      submit.textContent = "SENT!";
+
+      alert("Email successfully sent!");
+      setTimeout(() => submit.textContent = "SEND EMAIL", 2000);
+    })
+    .catch(error => {
+      console.log(error);
+    }); 
+    }
+  }
+};
+
+nameHolder.addEventListener("click", () => Name.focus());
+Name.addEventListener("focus", () => nameHolder.style.translate = "0 30px");
+Name.addEventListener("focusout", () => {
+  if (Name.value) {
+    return;
+  } else {
+    nameHolder.style.translate = "0 49px";
+  }
+});
+
+emailHolder.addEventListener("click", () => email.focus());
+email.addEventListener("focus", () => emailHolder.style.translate = "0 30px");
+email.addEventListener("focusout", () => {
+  if (email.value) {
+    return;
+  } else {
+    emailHolder.style.translate = "0 49px";
+  }
+});
+
+messageHolder.addEventListener("click", () => Name.focus());
+message.addEventListener("focus", () => messageHolder.style.translate = "0 33px");
+message.addEventListener("focusout", () => {
+  if (message.value) {
+    return;
+  } else {
+    messageHolder.style.translate = "0 134px";
+  }
+});
+
+submit.addEventListener("click", sendEmail);
